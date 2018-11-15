78cf8c52b4a60a62b01aac221380173ac3468d13
vannia <hvannia@gmail.com>
2018-11-15 15:07:07 -0800

    processing of poll data no print

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,41 +1,39 @@
 import os
 import csv
+import sys
+
+def printPollData(pollData):
+    totalVotes=0
+    winner=""
+    totalVotes= sum(pollData.values())
+    print('Election Results /n -------------------------')
+    print(f'Total Votes: {totalVotes}')
+    print('-------------------------')
+    for i in pollData:
+      # FIX THIS   print(f'{pollData.keys(i)} :  { pollData.values(i)/totalVotes} ({ pollData.values(i)})')
 
 
-#Open dataset for calculations
-fileName= "budget_data.csv"
+
+
+#read poll  CSV file
+fileName= "election_data.csv"
+pollData ={}
 with open(fileName) as f:
-    #pass
-    csvreader = csv.reader(f, delimiter=',')
+    dReader = csv.DictReader(f)
+    #read data, create new dict with new single candidate
     try:
+        for row in dReader:
+            if row['Candidate'] in pollData:
+                pollData[row['Candidate']]= pollData[row['Candidate']]+1
+            else:
+                pollData[row['Candidate']]=1
+        #print(pollData)
+        printPollData(pollData)
 
-        csv_header = next(csvreader)  # skip header
-
-        # do calculations for all rows after header
-        numOfMonths = 0
-        total =0
-
-        greatestIncrease =0.0
-        greatestDecrease = 0.0
-
-        avgChange = 0.0
-        previousValue = 0.0
-
-        for row in csvreader:
-            #print(row)
-            numOfMonths+=1
-            thisValue=float(row[1])
-            total= total + thisValue  # add up ammounts
-            if numOfMonths >=2:
-                avgChange = avgChange+ ( thisValue - previousValue )
-            previousValue = thisValue
-        avgChange = avgChange + ( thisValue - previousValue ) # last   <--- fix this
-ß
         # send print to fun
-        print (f'Total months :{numOfMonths} \nTotal :{int(total)}')
-        print(f'averageChange :{avgChange/numOfMonths}')
+
 
     #catch file error
     except csv.Error as e:
-        sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+        sys.exit('file %s, line %d: %s' % (fileName, dReader.line_num, e))
 
